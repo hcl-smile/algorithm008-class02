f25ac43b90592e4549f34639ff0da73258ade9ea
HCLTMAC <13215027210@163.com>
2020-04-15 17:23:27 +0800

    add: max-area

---
+++ Week_01/js/max-area/index.js
@@ -0,0 +1,68 @@
+/*
+ * [11] 盛最多水的容器
+ */
+
+/**
+ * 第一种方法
+ * 解题思路：双向夹逼方法
+ * 1.创建2个指针i和j，将数组分为左右2边
+ * 2.i得初始值为0，j得初始值为数组length - 1
+ * 3.当左边值小时，i加1
+ * 4.当右边值小时，j减1
+ * 5.最后对比出最大值
+ * 时间复杂度：0(n)
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  let i = 0;
+  let j = height.length - 1;
+
+  while (i < j) {
+    let min = Math.min(height[i], height[j]);
+    let areas = min * Math.abs(i - j);
+
+    max = Math.max(max, areas);
+
+    if (height[i] < height[j]) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+
+  return max;
+};
+
+/**
+ * 第二种方法
+ * 解题思路：两层嵌套循环
+ * 1.遍历每对组合
+ * 2.对比出每个组合得最大值
+ * 3.输出最大值
+ * 时间复杂度：0(n^2)
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  let len = height.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      let min = Math.min(height[i], height[j]);
+      max = Math.max(max, min * (j - i));
+    }
+  }
+
+  return max;
+};
--- Week_01/js/move-zeroes/index.js
+++ Week_01/js/move-zeroes/index.js
@@ -1,4 +1,5 @@
 /**
+ * 第一种方法
  * 解题思路：双指针
  * 1.创建一个新指针j
  * 2.遍历数组索引i，当nums[i]项不等于0，则替换nums[j] = nums[i]，同时j++
@@ -15,13 +16,70 @@ var moveZeroes = function (nums) {
     if (nums[i] !== 0) {
       if (i !== j) {
         nums[j] = nums[i];
+        nums[i] = 0;
       }
       j++;
     }
   }
+};
 
-  while (j < nums.length) {
-    nums[j] = 0;
-    j++;
+/**
+ * 第二种方法
+ * 解题思路：双指针快排
+ * 1.遍历数组，假如nums[i]不等于0，互换位置
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+
+      j++;
+    }
+  }
+};
+
+/**
+ * 第三种方法
+ * 解题思路：从右向左遍历
+ * 1.从右遍历数组，假如nums[i]等于0，则删除当前
+ * 2.数组向后推入0
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+  }
+};
+
+/**
+ * 第四种方法
+ * 解题思路：删除为0的项，将其重新往后推入数组
+ * 1.遍历数组，假如nums[i]等于0，则删除当前，同时把当前为0的项往后推入数组
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.push(nums.splice(i, 1));
+    }
   }
 };
