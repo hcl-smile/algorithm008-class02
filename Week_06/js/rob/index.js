fd463a1f79b597c569c3fd4ef1dfd35ffbcc9a68
HCLTMAC <13215027210@163.com>
2020-05-27 14:19:14 +0800

    add:添加打家劫舍

--- Week_06/js/rob/index.js
+++ Week_06/js/rob/index.js
@@ -6,6 +6,10 @@
 
 // @lc code=start
 /**
+ * 第一种方法
+ */
+
+/**
  * @param {number[]} nums
  * @return {number}
  */
@@ -26,5 +30,59 @@ var rob = function (nums) {
   }
 
   return max(a, b);
+};
+// @lc code=end
+
+// @lc code=start
+/**
+ * 第二种方法
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var rob = function (nums) {
+  let memo = [];
+
+  let recur = function (i) {
+    if (i >= nums.length) {
+      return 0;
+    }
+
+    if (memo[i] !== null) {
+      return memo[i];
+    }
+
+    memo[i] = Math.max(recur(i + 2) + nums[i], recur(i + 1));
+    return memo[i];
+  };
+
+  return recur(0);
+};
+// @lc code=end
+
+// @lc code=start
+/**
+ * 第三种方法
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var rob = function (nums) {
+  let memo = [];
+
+  memo[0] = 0;
+  memo[1] = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    memo[i + 1] = Math.max(memo[i], memo[i - 1] + nums[i]);
+  }
+
+  return memo[nums.length];
 };
 // @lc code=end
